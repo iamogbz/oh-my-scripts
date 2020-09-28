71a45b5b7c55e7ec6a871880d0416510b0c87225
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-09-28 00:55:10 -0400

    chore: wip

--- libraries/github-file.js
+++ libraries/github-file.js
@@ -41,7 +41,7 @@ class ExtendFilePreview {
     return this.safeFetch(
       isAbsolutePath(filePath) ? filePath : this.pathToBlob(filePath)
     )
-      .then((r) => r.text)
+      .then((r) => r.text())
       .catch((e) => {
         console.info(e);
         if (isAbsolutePath(filePath)) return null;
@@ -202,7 +202,7 @@ class ExtendFilePreview {
             {
               attributes: {
                 d: `M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45
-              1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z`,
+                1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z`,
                 "fill-rule": "evenodd",
               },
               tagName: "path",
