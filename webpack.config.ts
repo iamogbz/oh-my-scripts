3ba67f50fc71be80ac82334708b88981ba946952
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-10-16 06:08:01 -0400

    chore: split code into file chunks

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,71 +1,98 @@
 import * as path from "path";
 import * as fs from "fs";
-import { Configuration } from "webpack";
+import { Configuration, NormalModule } from "webpack";
 import { WebpackCompilerPlugin } from "webpack-compiler-plugin";
-import * as WebpackUserscript from "webpack-userscript";
 import { execSync } from "child_process";
 
-const isDevMode = process.env.NODE_ENV === "development";
-const outputFolder = path.resolve(__dirname, "dist");
-const projectsFolder = path.resolve(__dirname, "scripts");
-const projects = fs
-  .readdirSync(projectsFolder, { withFileTypes: true })
+const IS_DEV_MODE = process.env.NODE_ENV === "development";
+const FOLDER_RELEASE = path.resolve(__dirname, "dist");
+const FOLDER_SCRIPTS = path.resolve(__dirname, "scripts");
+const PROJECTS = fs
+  .readdirSync(FOLDER_SCRIPTS, { withFileTypes: true })
   .filter((entry) => entry.isDirectory())
   .map((dir) => dir.name);
-const entries = projects.reduce(
-  (acc, name) => ({
-    ...acc,
-    [name]: path.resolve(projectsFolder, name, "index.user.ts"),
-  }),
-  {}
-);
 
-export default {
-  devServer: {
-    contentBase: outputFolder,
-  },
-  entry: entries,
-  mode: isDevMode ? "development" : "production",
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules|bower_components)/,
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-typescript"],
+const CONFIG_SHARED: Configuration = {
+  mode: IS_DEV_MODE ? "development" : "production",
+};
+
+const CONFIGURATIONS: Configuration[] = [
+  {
+    ...CONFIG_SHARED,
+    entry: PROJECTS.reduce(
+      (acc, name) => ({
+        ...acc,
+        [`src/${name}`]: path.resolve(FOLDER_SCRIPTS, name, "index.user.ts"),
+      }),
+      {}
+    ),
+    module: {
+      rules: [
+        {
+          exclude: /(node_modules|bower_components)/,
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-typescript"],
+              },
+            },
+            "ts-loader",
+          ],
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        cacheGroups: {
+          lib: {
+            test: /[\\/]libraries[\\/](.*?).ts$/,
+            name(module: NormalModule) {
+              // get the name. E.g. libraries/fileName.ts
+              const libraryName = module.resource.match(
+                /[\\/]libraries[\\/](.*?).ts$/
+              )![1];
+              return `lib/${libraryName}`;
             },
           },
-          "ts-loader",
-        ],
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module: NormalModule) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )![1];
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm/${packageName.replace("@", "")}`;
+            },
+          },
+        },
+        maxInitialRequests: Infinity,
+        minSize: 0,
       },
-    ],
-  },
-  output: {
-    path: outputFolder,
-    filename: "[name].user.js",
-  },
-  plugins: [
-    new WebpackCompilerPlugin({
-      listeners: {
-        buildStart: () => execSync(`rm -rf ${outputFolder}`),
-      },
-      name: "Compiler",
-    }),
-    new WebpackUserscript({
-      headers: () => ({
-        namespace: "https://github.com/iamogbz/oh-my-scripts",
-        version: isDevMode ? `[version]-build.[buildNo]` : `[version]`,
+    },
+    output: {
+      filename: "[name].js",
+      path: FOLDER_RELEASE,
+    },
+    plugins: [
+      new WebpackCompilerPlugin({
+        listeners: {
+          buildStart: () => execSync(`rm -rf ${FOLDER_RELEASE}`),
+        },
+        name: "Compiler",
       }),
-      metajs: false,
-    }),
-  ],
-  resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx"],
-    fallback: { buffer: require.resolve("buffer/") },
-    modules: [path.resolve("."), path.resolve("./node_modules")],
+    ],
+    resolve: {
+      extensions: [".js", ".ts", ".jsx", ".tsx"],
+      fallback: { buffer: require.resolve("buffer/") },
+      modules: [path.resolve("."), path.resolve("./node_modules")],
+    },
+    target: "web",
   },
-  target: "web",
-} as Configuration;
+];
+
+export default CONFIGURATIONS;
