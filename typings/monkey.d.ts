19ed2dcb7b24e704d66ce8ee4951ecbc73aee1b6
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2023-08-24 02:50:17 +0000

    build: fix compile

--- typings/monkey.d.ts
+++ typings/monkey.d.ts
@@ -1,8 +1,10 @@
-// https://openuserjs.org/libs/Marti/GM_setStyle
-declare function GM_setStyle(params: { data: string }): void;
-// https://www.tampermonkey.net/documentation.php#GM_addStyle
-declare function GM_addStyle(css: string): void;
-// https://www.tampermonkey.net/documentation.php#GM_getResourceText
-declare function GM_getResourceText(name: string): string;
-// https://www.tampermonkey.net/documentation.php#GM_xmlhttpRequest
-declare function GM_xmlhttpRequest(details: Record<string, unknown>): void;
+declare interface Window {
+  // https://openuserjs.org/libs/Marti/GM_setStyle
+  GM_setStyle(params: { data: string }): void;
+  // https://www.tampermonkey.net/documentation.php#GM_addStyle
+  GM_addStyle(css: string): void;
+  // https://www.tampermonkey.net/documentation.php#GM_getResourceText
+  GM_getResourceText(name: string): string;
+  // https://www.tampermonkey.net/documentation.php#GM_xmlhttpRequest
+  GM_xmlhttpRequest(details: Record<string, unknown>): void;
+}
