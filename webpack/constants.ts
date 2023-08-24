c3924582802c9218a3b887d37c1dbd3392620356
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2023-08-23 23:15:13 -0400

    refactor: freeze constants

--- webpack/constants.ts
+++ webpack/constants.ts
@@ -1,34 +1,34 @@
 import * as path from "path";
 
-export const NodeEnv = {
+export const NodeEnv = Object.freeze({
   DEVELOPMENT: "development",
   PRODUCTION: "production",
-};
+});
 
-export const BuildStage = {
+export const BuildStage = Object.freeze({
   COMPILE: "compile",
   PACKAGE: "package",
-};
+});
 
 const resolve = (relative: string | TemplateStringsArray) =>
   path.resolve(process.cwd(), `${relative}`);
 
-export const Paths = {
+export const Paths = Object.freeze({
   COMPILE: resolve`tmp`,
   RELEASE: resolve`dist`,
   SCRIPTS: resolve`scripts`,
-};
+});
 
-export const Dists = {
+export const Dists = Object.freeze({
   LIB: "lib",
   NPM: "npm",
   SRC: "src",
-};
+});
 
-export const RegExps = {
+export const RegExps = Object.freeze({
   // get the name. E.g. node_modules/packageName/not/this/part.js
   // or node_modules/packageName
   NPM: /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
   // get the name. E.g. libraries/fileName.ts
   LIB: /[\\/]libraries[\\/](.*?)\.ts$/,
-};
+});
